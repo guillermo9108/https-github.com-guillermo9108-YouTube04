cad7ec086f38ca8dd6d6945d14ff3d921041715c
guillermo9108 <82621456+guillermo9108@users.noreply.github.com>
2026-04-10 21:24:22 -0400

    Last Sync: 2026-04-10 21:24 (Mobile)

---
+++ services/vectorService.ts
@@ -0,0 +1,9 @@
+
+/**
+ * Vector Logic - Placeholder
+ * Implementación desactivada para resolver errores de dependencias externas.
+ */
+export const searchVectors = async () => [];
+export const indexVector = async () => {};
+
+export default {};
